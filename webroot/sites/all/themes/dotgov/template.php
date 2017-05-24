02000824a3f1f952cb357382d71c4aebd163f345
Kapil-bulchandani <bkapil.jhu@gmail.com>
2017-05-24 09:26:05 -0400

    adding highcharts library locally

--- webroot/sites/all/themes/dotgov/template.php
+++ webroot/sites/all/themes/dotgov/template.php
@@ -4,7 +4,7 @@
  * The primary PHP file for this theme.
  */
 drupal_add_js('/sites/all/libraries/highcharts/highcharts.js');
-drupal_add_js('https://code.highcharts.com/highcharts-more.js');
-drupal_add_js('https://code.highcharts.com/modules/solid-gauge.js');
+drupal_add_js('/sites/all/libraries/highcharts/highcharts-more.js');
+drupal_add_js('/sites/all/libraries/highcharts/solid-gauge.js');
 drupal_add_js('https://www.gstatic.com/charts/loader.js');
 
