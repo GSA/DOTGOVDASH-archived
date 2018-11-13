8ae5ba968de74783b3f3167f05460498b994299e
Kapil-bulchandani <bkapil.jhu@gmail.com>
2018-11-13 02:30:54 -0500

    adding buttons to agency wide accessibility table

--- webroot/sites/all/modules/custom/dotgov_common/templates/dotgov-common-agencyacess-table.tpl.php
+++ webroot/sites/all/modules/custom/dotgov_common/templates/dotgov-common-agencyacess-table.tpl.php
@@ -1,36 +1,35 @@
-
-<!--
-<link rel="stylesheet" type="text/css" href="/css/result-light.css">
-
-<link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css">
-<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.1/css/jquery.dataTables.css">
-<script type="text/javascript" src="//cdn.datatables.net/1.10.1/js/jquery.dataTables.min.js"></script>
-<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/responsive/1.0.0/css/dataTables.responsive.css">-->
 <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-
 <script src="//cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js"></script>
-
+<script src="//cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
+<script src="//cdn.datatables.net/buttons/1.5.2/js/buttons.bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script src="//cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
+<script src="//cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
+<script src="//cdn.datatables.net/buttons/1.5.2/js/buttons.colVis.min.js"></script>
 
 <style type="text/css">
-   .dataTables_filter {
-	width: 50%;
-	float: right;
-	text-align: right;
-}
+    .dataTables_filter {
+        width: 50%;
+        float: right;
+        text-align: right;
+    }
 </style>
 <!-- TODO: Missing CoffeeScript 2 -->
 <script type="text/javascript">
-
-
-    
-        jQuery(document).ready(function() {
-            jQuery('#datatable-1').DataTable( {
-                responsive: true,
-                paging: false
-            } );
+    jQuery( document ).ready( function () {
+        jQuery( '#datatable-1' ).DataTable( {
+            responsive: true,
+            paging: false,
+            dom: 'Bfrtip',
+            buttons: [
+                'copy', 'csv', 'excel', 'pdf', 'print'
+            ]
         } );
 
 
+    } );
 </script>
 <div class="table-responsive">
     <div class="col-lg-12 col-sm-12 col-xs-12 nopadding" style="margin-bottom:15px;">
@@ -50,12 +49,12 @@
         </thead>
         <tbody>
         <?php
-        foreach($govwidedata['actualdata'] as $key=>$val){
-            print "<tr><td><a href='/agency_home/".$key."'>".$val['agencyname']."</a></td>";
-            print "<td>".$val['websitenos']."</td>";
-            print "<td>".(($val['colorcontr_avg'] != '')?$val['colorcontr_avg']:0)."</td>";
-            print "<td>".(($val['htmlattr_avg'] != '')?$val['htmlattr_avg']:0)."</td>";
-            print "<td>".(($val['missingimg_avg'] != '')?$val['missingimg_avg']:0)."</td>";
+        foreach ( $govwidedata[ 'actualdata' ] as $key => $val ) {
+            print "<tr><td><a href='/agency_home/" . $key . "'>" . $val[ 'agencyname' ] . "</a></td>";
+            print "<td>" . $val[ 'websitenos' ] . "</td>";
+            print "<td>" . ( ( $val[ 'colorcontr_avg' ] != '' ) ? $val[ 'colorcontr_avg' ] : 0 ) . "</td>";
+            print "<td>" . ( ( $val[ 'htmlattr_avg' ] != '' ) ? $val[ 'htmlattr_avg' ] : 0 ) . "</td>";
+            print "<td>" . ( ( $val[ 'missingimg_avg' ] != '' ) ? $val[ 'missingimg_avg' ] : 0 ) . "</td>";
             print "</tr>";
         }
 
@@ -64,4 +63,3 @@
         </tbody>
     </table>
 </div>
-
