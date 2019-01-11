d8c1c943049c65321f4f8f0c08c390c462b0daad
Ayaskant Sahu <ayas.kant@gmail.com>
2019-01-11 11:10:26 -0500

    added mobile chart to agency

--- webroot/sites/all/themes/dotgov/templates/views/views-view-unformatted--all-agency-data--page-14.tpl.php
+++ webroot/sites/all/themes/dotgov/templates/views/views-view-unformatted--all-agency-data--page-14.tpl.php
@@ -259,7 +259,10 @@ drupal_set_title($agencynode->title);
                                                               }
 
                                                               ?>
-                                                                <div class="views-field views-field-php-2 col-lg-6"><span class="field-content">Mobile Overall Average Score : <?php print $agency_mobovr_score ?> <br>
+                                                                <div class="views-field views-field-php-2 col-lg-6">
+                                                                  <div id="piechart1"></div>
+                                                                    <?php print $agencydata['ag_mob_chart']; ?>
+                                                                  <span class="field-content">Mobile Overall Average Score : <?php print $agency_mobovr_score ?> <br>
                                 Mobile Performance Score :
                                                                     <?= $agency_mobperf_score . ' (' . $mobperfmstat . ')' ?>
                                                                         <br>
