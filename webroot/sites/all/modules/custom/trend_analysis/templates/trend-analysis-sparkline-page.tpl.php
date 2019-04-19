c0835a96d6ef0a73ea35128ed39015e5d964f1b4
Steven Gebremariam <stevengm7@gmail.com>
2019-04-19 10:20:34 -0400

    Mobile score latest updates

--- webroot/sites/all/modules/custom/trend_analysis/templates/trend-analysis-sparkline-page.tpl.php
+++ webroot/sites/all/modules/custom/trend_analysis/templates/trend-analysis-sparkline-page.tpl.php
@@ -21,6 +21,13 @@ if (in_array($trend_vars['blockname'],$agency_color_arr)) {
     $negativecolor = '#4d525a';
     $positivecolor = '#4d525a';
 }
+
+$mobilePerf = array_slice($trend_vars['mobperfrm'], -5, 5);
+$mobilePerf = array_values($mobilePerf);
+$mobileUsab = array_slice($trend_vars['mobusab'], -5, 5);
+$mobileUsab = array_values($mobileUsab);
+$mobilePerfUsabScanDate = array_keys(array_slice($trend_vars['mobperfrm'], 0, 5));
+
 //if (in_array($trend_vars['blockname'],$score_arr)){
 //  $compliancetext = 'Score';
 //    $threshold = '75';
@@ -38,121 +45,163 @@ if (in_array($trend_vars['blockname'],$agency_color_arr)) {
 //    $threshold = '0';
 //}
 ?>
-<div id="<?=$trend_vars['container']?>" style="min-width: 150px; height: 40px; margin: 0 auto"></div>
-<script type="text/javascript">//<![CDATA[
 
-  Highcharts.chart('<?=$trend_vars['container']?>', {
-    chart: {
-          type: 'area',
-	events: {
-            load: function(){
-                var p = this.series[0].points[<?=(count($trend_vars['compliance'])-1)?>];
-                this.tooltip.refresh(p);
+<?php if($trend_vars['blockname'] == 'trends_mobile_spark'): ?>
+    <div id="<?=$trend_vars['container']?>" style="min-width: 150px; height: 200px; margin: 0 auto"></div>
+<?php else: ?>
+    <div id="<?=$trend_vars['container']?>" style="min-width: 150px; height: 40px; margin: 0 auto"></div>
+<?php endif; ?>
+
+<script type="text/javascript">
+//<![CDATA[
+    <?php if($trend_vars['blockname'] == 'trends_mobile_spark'): ?>
+        Highcharts.chart("<?=$trend_vars['container']?>", {
+          chart: {
+            type: 'column'
+          },
+          title: {
+            text: ''
+          },
+          subtitle: {
+            text: ''
+          },
+          xAxis: {
+            categories: <?php print json_encode($mobilePerfUsabScanDate); ?>,
+            crosshair: true
+          },
+          yAxis: {
+            min: 0,
+            max: 100,
+            title: {
+              text: 'Score (%)'
             }
-        }
-        },
-    title: {
-      useHTML: true,
-      text: '',
-      x: 0,
-    },
-        credits: {
-          enabled: false
-        },
-    xAxis: {
-      tooltip: {
-        formatter: function()
-        {
-            return 'Compliance';
-        }
-      },
-      lineWidth: 0,
-      minorGridLineWidth: 0,
-      lineColor: 'transparent',
-      minorTickLength: 0,
-      tickLength: 0,
-      labels:{
-        enabled:false//default is true
-      },
-      title: {
-        enabled: false,
-      },
-        categories: <?php echo json_encode($trend_vars['dates']); ?>,
-      },
-    yAxis: {
-      gridLineColor: 'transparent',
-      labels:{
-        enabled:false//default is true
-      },
-      title: {
-        enabled: false,
-        rotation: 0,
-      }
-    },
-<?php
-      if (in_array($trend_vars['blockname'],$agency_score_arr)){
-          ?>
-      tooltip: {
-          formatter: function()
-          {
-              if(this.y == -1)
-                  return 'Score: 0%';
-              else
-                  return 'Score: '+this.y +'%';
-          }
-      },
-      <?php
-      }
-         elseif($trend_vars['blockname'] == 'trends_mobile_spark'){
-             ?>
-      tooltip: {
-          formatter: function()
-          {
-              if(this.y == -1)
-                  return 'On '+this.x+' was Not Available';
-              else
-                  return 'On '+this.x+' Score was '+this.y;
-          }
-      },
-      <?php
-         }
-      elseif (!in_array($trend_vars['blockname'],$score_arr)){
-      ?>
-      tooltip: {
-          formatter: function()
-          {
-              if(this.y == -1)
-                  return 'On '+this.x+' was Not Compliant';
-              else if(this.y == '0')
-                  return 'On '+this.x+' was Not Available';
-              else
-                  return 'On '+this.x+' was Compliant';
-          }
-      },
-      <?php }
-      ?>
-      series: [{
-      threshold: <?=$threshold?>,
-      negativeColor: '<?=$negativecolor?>',
-      color: '<?=$positivecolor?>',
-      type: 'area',
-        name: '<?=$compliancetext?>',
-        showInLegend: false,
-        data: <?php echo json_encode($trend_vars['compliance'],JSON_NUMERIC_CHECK); ?>,
-    }],
-        lang: {
-        noData: "No Data to Show"
-    },
-    noData: {
-        style: {
-            fontWeight: 'bold',
-                fontSize: '15px',
-                color: '#303030'
-        }
-    }
-      });
-    //]]>
-
-  </script>
-
-
+          },
+          tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+          },
+          plotOptions: {
+            column: {
+              pointPadding: 0.2,
+              borderWidth: 0
+            }
+          },
+          series: [{
+            name: 'Performance',
+            data: <?php print json_encode($mobilePerf);?>
+        
+          }, {
+            name: 'Usability',
+            data: <?php print json_encode($mobileUsab);?>
+        
+          }]
+        });
+    <?php else: ?>
+        Highcharts.chart('<?=$trend_vars['container']?>', {
+            chart: {
+                type: 'area',
+                events: {
+                    load: function(){
+                        var p = this.series[0].points[<?=(count($trend_vars['compliance'])-1)?>];
+                        this.tooltip.refresh(p);
+                    }
+                }
+            },
+          
+            title: {
+              useHTML: true,
+              text: '',
+              x: 0,
+            },
+          
+            credits: {
+              enabled: false
+            },
+          
+            xAxis: {
+                tooltip: {
+                    formatter: function() {
+                        return 'Compliance';
+                    }
+                },
+                lineWidth: 0,
+                minorGridLineWidth: 0,
+                lineColor: 'transparent',
+                minorTickLength: 0,
+                tickLength: 0,
+                labels:{
+                    enabled:false//default is true
+                },
+                title: {
+                    enabled: false,
+                },
+                categories: <?php echo json_encode($trend_vars['dates']); ?>,
+            },
+            yAxis: {
+                gridLineColor: 'transparent',
+                labels:{
+                    enabled:false//default is true
+                },
+                title: {
+                    enabled: false,
+                    rotation: 0,
+                }
+            },
+          
+            <?php if (in_array($trend_vars['blockname'],$agency_score_arr)): ?>
+                tooltip: {
+                    formatter: function() {
+                        if(this.y == -1)
+                            return 'Score: 0%';
+                        else
+                            return 'Score: '+this.y +'%';
+                    }
+                },
+            <?php elseif($trend_vars['blockname'] == 'trends_mobile_spark'): ?>
+                tooltip: {
+                    formatter: function() {
+                        if(this.y == -1)
+                            return 'On '+this.x+' was Not Available';
+                        else
+                            return 'On '+this.x+' Score was '+this.y;
+                    }
+                },
+            <?php elseif (!in_array($trend_vars['blockname'],$score_arr)): ?>
+                tooltip: {
+                    formatter: function() {
+                        if(this.y == -1)
+                            return 'On '+this.x+' was Not Compliant';
+                        else if(this.y == '0')
+                            return 'On '+this.x+' was Not Available';
+                        else
+                            return 'On '+this.x+' was Compliant';
+                    }
+                },
+            <?php endif; ?>
+            series: [{
+                threshold: <?=$threshold?>,
+                negativeColor: '<?=$negativecolor?>',
+                color: '<?=$positivecolor?>',
+                type: 'area',
+                name: '<?=$compliancetext?>',
+                showInLegend: false,
+                data: <?php echo json_encode($trend_vars['compliance'],JSON_NUMERIC_CHECK); ?>,
+            }],
+            lang: {
+                noData: "No Data to Show"
+            },
+            noData: {
+                style: {
+                    fontWeight: 'bold',
+                    fontSize: '15px',
+                    color: '#303030'
+                }
+            }
+        });
+    <?php endif; ?>
+//]]>
+</script>
