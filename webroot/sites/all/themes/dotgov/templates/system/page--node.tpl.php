ce62ee953b5b678d33bcf745e08f67dc6925d27f
Ayaskant Sahu <asahu@doc.gov>
2019-05-01 18:42:40 -0400

    Fixed search results and scan results page. ticket WEB-3857

--- webroot/sites/all/themes/dotgov/templates/system/page--node.tpl.php
+++ webroot/sites/all/themes/dotgov/templates/system/page--node.tpl.php
@@ -72,6 +72,9 @@
  *
  * @ingroup templates
  */
+if (strpos($title, 'HTTPS DAP') !== false) {
+    $title = str_replace('HTTPS DAP','HTTPS & DAP',$title);
+}
 ?>
 <div class="top-bar">
 			<!-- top header-->
@@ -148,7 +151,7 @@
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
+        <h1 class="page-header"><?php print $title; ?>(Raw Scan Output Page)</h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
   <div class="container white-back">
