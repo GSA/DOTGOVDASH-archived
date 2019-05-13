78dfcd1fa22cd31393aa574aabc6be8a669f2893
Ayaskant Sahu <ayaskant.sahu@gsa.gov>
2019-05-12 23:43:34 -0400

    changes for empty graph

--- webroot/sites/all/modules/custom/dotgov_common/templates/dotgov-common-searchstatus-graph.tpl.php
+++ webroot/sites/all/modules/custom/dotgov_common/templates/dotgov-common-searchstatus-graph.tpl.php
@@ -1,3 +1,13 @@
+<?php
+if(isset($agencydata['search_available']))
+  $search_available = $agencydata['search_available'];
+else
+  $search_available = 0;
+if(isset($agencydata['search_notavailable']))
+  $search_notavailable = $agencydata['search_notavailable'];
+else
+  $search_notavailable = 0;
+?>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script language="JavaScript">
     google.charts.load('current', {'packages':['corechart']});
@@ -7,8 +17,8 @@
 
         var data = google.visualization.arrayToDataTable([
             ['Type', 'Number'],
-            ['Search Available',     <?=$agencydata['search_available']?>],
-            ['Search Not Available',      <?=$agencydata['search_notavailable']?>],
+            ['Search Available',     <?=$search_available?>],
+            ['Search Not Available',      <?=$search_notavailable?>],
         ]);
         var options = {
             title: 'Search Engine Status Breakdown',
