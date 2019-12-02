744e3fd252aa097a675d53437556808f6aaaf31b
AK SAHU <ayaskant.sahu@gsa.gov>
2019-12-02 00:07:37 -0500

    added email functionality

--- scripts/scanEngine.php
+++ scripts/scanEngine.php
@@ -10,15 +10,6 @@ include_once("../scripts/commonScanFunctions.php");
 //Before Starting scan get the latest website listing and data from pulse
 writeToLogs("Get Latest Websites and data from Pulse\n",$logFile);
 getPulseData();
-runSearchEngineScan();
-//Update Search engine table with custom collected data from search_customidentfied_domains.csv
-exec("drush sql-query \"create table if not exists tmpdomains(domain varchar(255),search_available varchar(11));\"");
-exec("drush sql-query \"truncate tmpdomains;\"");
-exec("drush sql-query \"load data local infile '../scripts/search_customidentfied_domains.csv' into table tmpdomains FIELDS TERMINATED BY ',';\"");
-exec("drush sql-query \"update search_scan a, tmpdomains b set a.search_available=b.search_available where a.domain=b.domain;\"");
-exec("drush sql-query \"update search_scan set search_available = 'NA',type = NULL where domain in (select a.title from node a, field_data_field_redirect b, field_data_field_website_id c where a.nid=c.field_website_id_nid and b.entity_id=c.entity_id and b.field_redirect_value='yes');\"");
-exec("drush sql-query \"drop table tmpdomains;\"");
-
 //Start the scan and get the scan id.
 writeToLogs("Starting Scan",$logFile);
 $scanId = startScan();
@@ -57,8 +48,5 @@ updateAccessibilityScanCustom($website['domain'],$scanId);
     //After scan is done. Run the taxonomy processor. This will parse all taxonomy data and create/edit taxonomies and tag appropriate content.
 archiveGovwideTrendData();
 archiveAgencywideTrendData();
-
-// This will update 'scans' content type scanEndTime
-updateScanEndDateTime();
 
 print "Finished Processing!\n";
