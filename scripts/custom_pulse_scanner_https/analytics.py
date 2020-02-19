4e6c047f20d7f7fde982dcb7e869d7ff2e69b2d5
kadem1 <kalid.adem@gsa.gov>
2020-02-19 17:09:15 -0600

    Initial Commit for analytics.py to produce analytics.csv

---
+++ scripts/custom_pulse_scanner_https/analytics.py
@@ -0,0 +1,29 @@
+import requests, csv
+base = "https://api.gsa.gov/analytics/dap/v1.1/domain/"
+route = "/reports/site/data?"
+key = "api_key=CUluXOXEJqFO4Hqlt4YJpAlE8zua2rP3nUcIM2rZ"
+param = "&limit=1"
+CSV_URL = "https://raw.githubusercontent.com/GSA/data/master/dotgov-domains/current-federal.csv"
+
+#download csv file contents
+with requests.Session() as s:
+    download = s.get(CSV_URL)
+    decoded_content = download.content.decode('utf-8')
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
+    my_list.sort()
+
+#empty csv output file
+open("analytics.csv", "w").close()
+f=open("analytics.csv", "a+")
+f.write("Domain Name,Participates in Dap" + "\n")
+f.close()
+for row in my_list:
+    domain = row[0].lower() #convert domains to lowercase
+    url = base + domain + route + key + param #construct url
+    response = requests.get(url, verify=False).text #sends request
+
+    #write result
+    f=open("analytics.csv", "a+")
+    f.write(domain + ",Yes\n" if "{\"id\":" in response else domain + ",No\n") #in dap or not
+    f.close()
