3d305b756d3552d5ad92906bee600b9e66819fa5
kadem1 <kalid.adem@gsa.gov>
2020-03-02 09:40:04 -0500

    Reduce domains to executive agencies only. Added more fields to match pulse format. Increased from 1000 to 10000 records from open gsa api. Scan uses data from the past 10 days now

--- scripts/custom_pulse_scanner_analytics/secondLevelDomain.py
+++ scripts/custom_pulse_scanner_analytics/secondLevelDomain.py
@@ -1,22 +1,39 @@
 import requests, csv, sys
+from datetime import datetime, timedelta
 
-response = requests.get('https://api.gsa.gov/analytics/dap/v1.1/reports/second-level-domain/data?api_key=CUluXOXEJqFO4Hqlt4YJpAlE8zua2rP3nUcIM2rZ&after=2020-02-01', verify=False)
-contents = response.text.replace('{', '\n{').replace('}]', '}\n]')
-CSV_URL = "https://raw.githubusercontent.com/GSA/data/master/dotgov-domains/current-federal.csv" #source for domain list
+allDomains = "https://raw.githubusercontent.com/GSA/data/master/dotgov-domains/current-federal.csv" #source for domain list
+data = []
+
 #download csv file contents
 with requests.Session() as s:
-    download = s.get(CSV_URL)
+    download = s.get(allDomains)
     decoded_content = download.content.decode('utf-8')
     cr = csv.reader(decoded_content.splitlines(), delimiter=',')
     my_list = list(cr)
     my_list.sort()
-open("analyticsSecondLevelDomain.csv", 'w').close()
+n_list = [] #new list to include only executive agency domains
+for row in my_list:
+    if "Federal Agency - Executive".lower() == row[1].lower():
+        n_list.append(row)
+# print(n_list)
+
+days_to_subtract = 10 #how many days back to go for scan, default is 10
+limit = 10000 #limit of results return, default is 10000
+
+date = (datetime.today() - timedelta(days=days_to_subtract)).strftime("%Y-%m-%d")
+response = requests.get('https://api.gsa.gov/analytics/dap/v1.1/reports/second-level-domain/data?api_key=CUluXOXEJqFO4Hqlt4YJpAlE8zua2rP3nUcIM2rZ&after=' + date + '&limit=' + str(limit) + '', verify=False)
+contents = response.text.replace('{', '\n{').replace('}]', '}\n]')
+open("analyticsSecondLevelDomain.csv", 'w').close() #empty file
+f = open("analyticsSecondLevelDomain.csv", mode='a')
+f.write('"Domain","Base Domain","URL","Agency","Sources","Participates in DAP?"')
+f.close()
 
 #using source, check if domain present in list and write results to
-for r in my_list:
+for r in n_list:
+    url = r[0].lower()
     with open('analyticsSecondLevelDomain.csv', mode='a') as ana_csv:
-        analytics_writer = csv.writer(ana_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        analytics_writer = csv.writer(ana_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
         if "\"" + r[0].lower() + "\"" in contents:
-            analytics_writer.writerow([r[0], "True"])
+            analytics_writer.writerow([url, url, "https://"+url, r[3], "dotgov", "Yes"])
         else:
-            analytics_writer.writerow([r[0], "False"])
+            analytics_writer.writerow([url, url, "https://"+url, r[3], "dotgov", "No"])
