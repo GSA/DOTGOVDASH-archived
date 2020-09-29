d262e59846cf52ce62ef94c7372b89f1a16c0d66
anveshgsa <anvesh.vankalapati@gsa.gov>
2020-09-29 13:23:15 -0400

    remove  unnecessary meta

--- webroot/sites/all/themes/dotgov/templates/system/html.tpl.php
+++ webroot/sites/all/themes/dotgov/templates/system/html.tpl.php
@@ -57,7 +57,6 @@
 <html<?php print $html_attributes;?><?php print $rdf_namespaces;?>>
 <head>
   <link rel="profile" href="<?php print $grddl_profile; ?>" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
