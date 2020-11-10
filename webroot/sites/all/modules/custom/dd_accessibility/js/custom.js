985910eabc78a79b6a91e9442be06a02db70cc44
anveshgsa <anvesh.vankalapati@gsa.gov>
2020-11-10 15:54:18 -0500

    temperorly disable sort logic

--- webroot/sites/all/modules/custom/dd_accessibility/js/custom.js
+++ webroot/sites/all/modules/custom/dd_accessibility/js/custom.js
@@ -181,35 +181,35 @@
 
 
           //column sort logic
-          if (sortInfo.colIndex >= 0 && sortInfo.direction != 0) {
-            var f = [], d, a = sortInfo.direction, c = sortInfo.colIndex;
-            b = this;
-            b.sorted = !1;
-            var h = b.getRowKeys()
-              , e = b.getColKeys();
-            if (sortInfo.colKey !== JSON.stringify(e[c])) {
-              sortInfo.colIndex = -1;
-              sortInfo.direction = 0;
-              sortInfo.colKey = null;
-            } else {
-              for (d in h) {
-                var p = h[d];
-                var n = null != c ? e[c] : [];
-                n = b.getAggregator(p, n);
-                f.push({
-                  val: n.value(),
-                  key: p
-                })
-              }
-              f.sort(function (b, c) {
-                return a * $.pivotUtilities.naturalSort(b.val, c.val)
-              });
-              b.rowKeys = [];
-              for (d = 0; d < f.length; d++)
-                b.rowKeys.push(f[d].key);
-              b.sorted = !0
-            }
-          }
+          // if (sortInfo.colIndex >= 0 && sortInfo.direction != 0) {
+          //   var f = [], d, a = sortInfo.direction, c = sortInfo.colIndex;
+          //   b = this;
+          //   b.sorted = !1;
+          //   var h = b.getRowKeys()
+          //     , e = b.getColKeys();
+          //   if (sortInfo.colKey !== JSON.stringify(e[c])) {
+          //     sortInfo.colIndex = -1;
+          //     sortInfo.direction = 0;
+          //     sortInfo.colKey = null;
+          //   } else {
+          //     for (d in h) {
+          //       var p = h[d];
+          //       var n = null != c ? e[c] : [];
+          //       n = b.getAggregator(p, n);
+          //       f.push({
+          //         val: n.value(),
+          //         key: p
+          //       })
+          //     }
+          //     f.sort(function (b, c) {
+          //       return a * $.pivotUtilities.naturalSort(b.val, c.val)
+          //     });
+          //     b.rowKeys = [];
+          //     for (d = 0; d < f.length; d++)
+          //       b.rowKeys.push(f[d].key);
+          //     b.sorted = !0
+          //   }
+          // }
         }
 
         return results;
@@ -445,6 +445,7 @@
         if (col >= opts.collapseAt) {
           arrow = arrowCollapsed + " ";
           hClass = classCollapsed;
+          ah.th.setAttribute('root-node', '1');
           ah.clickStatus = clickStatusCollapsed;
           ah.onClick = expandAxis;
         }
@@ -1353,8 +1354,8 @@
           buildColTotals(tr, colAttrHeaders, rowAttrs, colAttrs, opts);
         }
         buildGrandTotal(tbody, tr, rowAttrs, colAttrs, opts);
-        collapseAxis(colAxisHeaders, opts.colSubtotalDisplay.collapseAt, colAttrs, opts.colSubtotalDisplay);
-        collapseAxis(rowAxisHeaders, opts.rowSubtotalDisplay.collapseAt, rowAttrs, opts.rowSubtotalDisplay);
+        //collapseAxis(colAxisHeaders, opts.colSubtotalDisplay.collapseAt, colAttrs, opts.colSubtotalDisplay);
+        //collapseAxis(rowAxisHeaders, opts.rowSubtotalDisplay.collapseAt, rowAttrs, opts.rowSubtotalDisplay);
         result.setAttribute("data-numrows", rowKeys.length);
         result.setAttribute("data-numcols", colKeys.length);
         result.style.display = "";
@@ -1677,7 +1678,14 @@
     0 != b.length && (c = c ? b.closest(".pvtFixedHeaderOuterContainer") : b.parent(),
       a = "object" === typeof a ? a : {},
       this.fixedHeaders && this.fixedHeaders.destroy(),
-      this.fixedHeaders = new PivotTableWrapper(c, b, !0 === a.smooth ? !0 : !1, !1 === a.rows ? !1 : !0, !1 === a.columns ? !1 : !0, a.disableByAreaFactor ? a.disableByAreaFactor : .5, !1 === a.useSticky ? !1 : !0))
+      // this.fixedHeaders = new PivotTableWrapper(c, b, !0 === a.smooth ? !0 : !1, !1 === a.rows ? !1 : !0, !1 === a.columns ? !1 : !0, a.disableByAreaFactor ? a.disableByAreaFactor : .5, !1 === a.useSticky ? !1 : !0))
+
+      this.fixedHeaders = new PivotTableWrapper(c, b, !0 === a.smooth ? !0 : !1, !1 === a.rows ? !1 : !0, !1 === a.columns ? !1 : !0, a.disableByAreaFactor ? a.disableByAreaFactor : .5, !1 === a.useSticky ? !1 : !0));
+
+
+      setTimeout(function(){
+        $("th[root-node='1']:first").click();
+      }, 1);
   };
 
   window.PivotTableExtensions.defaults = {
