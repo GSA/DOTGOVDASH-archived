6112c1449fc5e52af9d69df5f4083415ea373295
Anvesh Vankalapati <anvesh.vankalapati@gsa.gov>
2021-01-25 16:10:09 -0500

    updated 508 issues

--- webroot/sites/all/themes/dotgov/templates/views/views-view-unformatted--all-agency-data--page-14.tpl.php
+++ webroot/sites/all/themes/dotgov/templates/views/views-view-unformatted--all-agency-data--page-14.tpl.php
@@ -175,7 +175,7 @@ if ($agencynode->field_agency_logo['und'][0]['uri'] != '') {
                                         <div id="piechartmob" style="margin-top:-17px;height:140px;"></div>
                                         <?php print $agencydata['ag_mob_chart'];?>
                                       </div>
-                                      <table style="width:100%">
+                                      <table style="width:100%" aria-label="Mobile Performance Breakdown Table"> 
                                         <th style="background-color: #215393;color: white;"> Breakdown </th>
                                         <th style="background-color: #215393;color: white;"> Websites </th>
                                         <tr>
@@ -202,7 +202,7 @@ if ($agencynode->field_agency_logo['und'][0]['uri'] != '') {
                                         <div id="piechartmobusab" style="margin-top:-17px;height:140px;"></div>
                                         <?php print $agencydata['ag_mob_usab_chart'];?>
                                       </div>
-                                      <table style="width:100%">
+                                      <table style="width:100%" aria-label="Mobile Usability Breakdown Table">
                                         <th style="background-color: #215393;color: white;"> Breakdown </th>
                                         <th style="background-color: #215393;color: white;"> Websites </th>
                                         <tr>
@@ -225,7 +225,7 @@ if ($agencynode->field_agency_logo['und'][0]['uri'] != '') {
                               <div class="view-button clearfix">
                                 <div class="row text-center">
 <!--                                  <a class="" href="/website/mobile/reports"> <img src="/sites/all/themes/dotgov/images/DD-btn_full_report.png" width="" height="25" alt=""/></a>-->
-                                  <a href="/improve-my-score"><img src="/sites/all/themes/dotgov/images/DD-btn_imp_scores.png" width="" height="30" alt=""/></a>
+                                  <a href="/improve-my-score" title="link for improve my score"><img src="/sites/all/themes/dotgov/images/DD-btn_imp_scores.png" width="" height="30" alt=""/></a>
                                 </div>
                               </div>
                             </div>
@@ -315,7 +315,7 @@ Above graph shows the breakdown of Accessibility Issues by category</span></div>
                                     </div>
                                     <div class="view-button">
                                         <div class="row col-xs-12 nopadding">
-                                            <div class="col-xs-12 col-lg-12 text-center"> <a href="/improve-my-score"><img src="/sites/all/themes/dotgov/images/DD-btn_imp_scores.png" width="245" height="30" alt=""/></a> </div>
+                                            <div class="col-xs-12 col-lg-12 text-center"> <a href="/improve-my-score" title="link for improve my score"><img src="/sites/all/themes/dotgov/images/DD-btn_imp_scores.png" width="245" height="30" alt=""/></a> </div>
                                         </div>
 
                                     </div>
@@ -350,7 +350,7 @@ Above graph shows the breakdown of Accessibility Issues by category</span></div>
                                                         <div class="view-wrapper" style="min-height:325px">
                                                             <div class="col-xs-12 col-md-12 col-lg-6 grey-gradient" style="height:165px;">
                                                                 <h5>DNSSEC Score Breakdown</h5>
-                                                                <table width="100%" class="dnssec-table">
+                                                                <table width="100%" class="dnssec-table" aria-label="DNSSEC Score Breakdown Table">
 
                                                                     <th style="background-color: #215393;color: white;">Breakdown</th>
                                                                     <th style="background-color: #215393;color: white;">Websites</th>
@@ -463,7 +463,7 @@ print "$output4<br><div class='col-lg-12 text-center clearfix'><span style='colo
 
 
                                                             <div class="row col-xs-12 nopadding">
-                                                                <div class="col-xs-12 col-lg-12 text-center"> <a href="/improve-my-score"><img src="/sites/all/themes/dotgov/images/DD-btn_imp_scores.png" width="245" height="30" alt=""/></a> </div>
+                                                                <div class="col-xs-12 col-lg-12 text-center"> <a href="/improve-my-score" title="link for improve my score"><img src="/sites/all/themes/dotgov/images/DD-btn_imp_scores.png" width="245" height="30" alt=""/></a> </div>
                                                             </div>
 
                                                         </div>
@@ -497,7 +497,7 @@ print "$output4<br><div class='col-lg-12 text-center clearfix'><span style='colo
                                                     <?php print $agencydata['searchengines_graph'];
 //print "<span style='color:#29643a; font-size: 12px;font-style: italic;'>Above graph shows the breakdown of On-Site Search Engines</span>";
 ?>
-                                                    <table style="width:100%">
+                                                    <table style="width:100%" aria-label="On-Site Search Engine Status Table">
                                                         <tr style="background-color: #215393;color: white;">
                                                             <td>On-Site Search Engine</td>
                                                             <td>&nbsp;Total</td>
@@ -516,7 +516,7 @@ foreach ($agencydata['searchenginedata'] as $skey => $sval) {
 //print "<span style='color:#29643a; font-size: 12px;font-style: italic;'>Above graph shows the breakdown of On-Site Search Engines by category</span>";
 $searchenginestatus = $agencydata['searchenginestatus'];
 ?>
-                                                    <table>
+                                                    <table  aria-label="On-Site Search Engine Breakdown Table">
                                                         <tr style="background-color: #215393;color: white;">
                                                             <td> On-Site Search Available</td>
                                                             <td>On-Site Search Not Available</td>
@@ -533,7 +533,7 @@ $searchenginestatus = $agencydata['searchenginestatus'];
                                         </div>
                                     </div>
                                     <div class="view-button clearfix"><div class="row col-xs-12 nopadding">
-                                            <div class="col-xs-12 col-lg-12 text-center"> <a href="/improve-my-score"><img src="/sites/all/themes/dotgov/images/DD-btn_imp_scores.png" width="245" height="30" alt=""/></a> </div>
+                                            <div class="col-xs-12 col-lg-12 text-center"> <a href="/improve-my-score" title="link for improve my score"><img src="/sites/all/themes/dotgov/images/DD-btn_imp_scores.png" width="245" height="30" alt=""/></a> </div>
                                         </div>
                                     </div>
                                 </div>
@@ -650,7 +650,7 @@ $searchenginestatus = $agencydata['searchenginestatus'];
                                                     </script>
                                                 </div>
                                             </div>
-                                            <table width="100%">
+                                            <table width="100%" aria-label="HTTPS score breakdown Table">
 
                                                 <th style="background-color: #215393;color: white;">Criteria</th>
                                                 <th style="background-color: #215393;color: white">Supporting Websites </th>
@@ -811,7 +811,7 @@ print "$output3 <span class='col-xs-12 text-center clearfix' style='color: " . d
                                                                         );
                                                                     </script>
                                                                 </div></div>
-                                                            <table width="100%">
+                                                                <table width="100%" aria-label="M-15-13 and BOD 18-01 score breakdown Table">
 
                                                                 <th style="background-color: #215393;color: white;"> Breakdown </th>
                                                                 <th style="background-color: #215393;color: white;"> Websites </th>
@@ -959,7 +959,7 @@ print "$output2 <span class='col-xs-12 text-center'style='color: " . dotgov_comm
                                                                             );
                                                                         </script>
                                                                     </div></div>
-                                                                <table width="100%">
+                                                                    <table width="100%" aria-label="IPV6 Score Breakdown Table">
 
                                                                     <th style="background-color: #215393;color: white;"> Breakdown </th>
                                                                     <th style="background-color: #215393;color: white;"> Websites </th>
@@ -1119,7 +1119,7 @@ print "$output7 <span class='col-xs-12 nopadding text-center' style='color: " . 
                                                         </script>
                                                     </div>
                                                         </div>
-                                                    <table style="width:100%;">
+                                                        <table style="width:100%;" aria-label="DAP Information Breakdown Table">
 
                                                         <th style="background-color: #215393;color: white;border: 1px;"> Breakdown </th>
                                                         <th style="background-color: #215393;color: white;border: 1px;"> Websites </th>
@@ -1267,7 +1267,7 @@ print "$output6 <br><span class='col-xs-12 clearfix text-center' style='color: "
                                                 </script>
                                             </div>
                                                 </div>
-                                            <table width="100%">
+                                                <table width="100%" aria-label="Free of RC4/3DES and SSLv2/SSLv3 score breakdown Table">
 
                                                 <th style="background-color: #215393;color: white;"> Breakdown </th>
                                                 <th style="background-color: #215393;color: white;"> Websites </th>
@@ -1365,7 +1365,7 @@ print "$output5<br><span class='text-center col-xs-12 nopadding' style='color: "
                                                 </script>
                                             </div>
 
-                                                <table style="width:100%;">
+                                            <table style="width:100%;" aria-label="USWDS Code Table">
 
                                                     <th style="background-color: #215393;color: white;border: 1px;"> Breakdown </th>
                                                     <th style="background-color: #215393;color: white;border: 1px;"> Websites </th>
