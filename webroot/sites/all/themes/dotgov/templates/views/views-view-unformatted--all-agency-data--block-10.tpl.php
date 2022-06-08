e95d16f4d3150d7c38981e80a56201ccce9b7a81
Ayaskant Sahu <ayaskant.sahu@gsa.gov>
2022-06-08 09:29:30 -0400

    Changed urls to new report

--- webroot/sites/all/themes/dotgov/templates/views/views-view-unformatted--all-agency-data--block-10.tpl.php
+++ webroot/sites/all/themes/dotgov/templates/views/views-view-unformatted--all-agency-data--block-10.tpl.php
@@ -53,4 +53,4 @@ foreach($view->style_plugin->rendered_fields[0] as $key=>$val){
 </script>
 <div id="columnchart_material" ></div>
 <span class="field-content col-lg-12"><a href="/content/scoring-methods" title="" data-toggle="tooltip" class="infor" data-original-title="Click Here to see the scoring methods used to calculate the scores"><i class="icon glyphicon glyphicon-info-sign"></i><span class="sr-only">Click Here to see the scoring methods used to calculate the scores</span></a></span>
-<a id="link-all-reports" href="/website/all/reports">Complete List</a>
+<a id="link-all-reports" href="/website/all/reports-new">Complete List</a>
