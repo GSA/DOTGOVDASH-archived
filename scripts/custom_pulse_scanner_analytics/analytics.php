352323fd915bfd1fd2767b9a42ea4100a4f53e21
AK SAHU <ayaskant.sahu@gsa.gov>
2022-06-22 10:04:13 -0500

    fixed dynamic date

--- scripts/custom_pulse_scanner_analytics/analytics.php
+++ scripts/custom_pulse_scanner_analytics/analytics.php
@@ -1,10 +1,15 @@
 <?php
-$allDomains = file("https://raw.githubusercontent.com/cisagov/dotgov-data/main/current-federal.csv");
+#$allDomains_main = file("https://raw.githubusercontent.com/cisagov/dotgov-data/main/current-federal.csv");
+#$allDomains_army = file("https://b81c0de9f2cd4c6a090c2882f5f7e3e70bbf003d@raw.githubusercontent.com/GSA/DOTGOVDASH/master/custom_domains.csv");
+#$allDomains = array_merge($allDomains_main, $allDomains_army);
+$allDomains = file("/tmp/current-federal.csv");
 $data = [];
 foreach ($allDomains as $line) {
   $data = str_getcsv($line);
-  if($data[1] == "Federal Agency - Executive"){
+  if($data[1] == "Federal - Executive"){
     $domainname = strtolower($data[0]);
+//For army domains forcibly append www
+  $domainname = str_replace("www.","",$domainname);
     $alldomains[]= $domainname;
     $alldomain_agency[$domainname] = $data[3];
     //	print $domainname."\n";
@@ -12,23 +17,37 @@ foreach ($allDomains as $line) {
 }
 $alldomains = array_unique($alldomains);
 //print "\n".count($alldomains)."\n";
-$json = file_get_contents("https://api.gsa.gov/analytics/dap/v1.1/reports/second-level-domain/data?api_key=CUluXOXEJqFO4Hqlt4YJpAlE8zua2rP3nUcIM2rZ&after=2020-02-01&limit=10000");
+$date = date("Y-m-d");
+$querydate = date("Y-m-d",strtotime(date("Y-m-d", strtotime($date)) . " -15 days"));
+$json = file_get_contents("https://api.gsa.gov/analytics/dap/v1.1/reports/second-level-domain/data?api_key=CUluXOXEJqFO4Hqlt4YJpAlE8zua2rP3nUcIM2rZ&after=".$querydate."&limit=100000");
+#print $json;
 $domArr = json_decode($json);
 foreach($domArr as $key=>$val){
-  $domainNames[] = trim($val->domain);
+  $domval = str_replace("www.","",$val->domain);
+  #print $domval."\n";
+  $domainNames[] = trim($domval);
   //print $val->domain."\n";
 }
 $domainNames = array_unique($domainNames);
-print "\n".count($domainNames)."\n";
+#print "\n".count($domainNames)."\n";
 print '"Domain","Base Domain","URL","Agency","Sources","Participates in DAP?"';
 print "\n";
 foreach($alldomains as $key=>$val){
-  $domurl = "https://".$val;
+  if(strpos($val,".mil") !== false){
+   if(strpos($val,"www.") === false)
+    $domainname = "www.".$val;
+   else
+    $domainname = $val;
+  }
+  else
+    $domainname = $val;
+  $domurl = "https://".$domainname;
+
   if(in_array($val,$domainNames)){
-    print "\"".$val."\",\"".$val."\",\"".$domurl."\",\"".$alldomain_agency[$val]."\",\"dotgov\",\"Yes\" \n";
+    print "\"".$domainname."\",\"".$domainname."\",\"".$domurl."\",\"".$alldomain_agency[$val]."\",\"dotgov\",\"Yes\"\n";
   }
   else{
-    print "\"".$val."\",\"".$val."\",\"".$domurl."\",\"".$alldomain_agency[$val]."\",\"dotgov\",\"No\" \n";
+    print "\"".$domainname."\",\"".$domainname."\",\"".$domurl."\",\"".$alldomain_agency[$val]."\",\"dotgov\",\"No\"\n";
   }
 }
 ?>
